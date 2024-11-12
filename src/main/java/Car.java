ac075534c7b30d69e63f85ba665b96526940e4ba
vladyulik <vanya123wow321@gmail.com>
2024-11-12 05:54:42 +0200

    Add doc comments for car class

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,5 +1,10 @@
 import java.util.Objects;
 
+/**
+ * Represents a Car with properties such as build year, maximum speed, model, color,
+ * insurance status, and damage status. The Car class provides methods for comparing
+ * cars by build year and maximum speed, and for checking equality based on all fields.
+ */
 public class Car implements Comparable<Car> {
     private int buildYear;
     private int maxSpeed;
@@ -8,6 +13,16 @@ public class Car implements Comparable<Car> {
     private boolean isInsured;
     private boolean isDamaged;
 
+    /**
+     * Constructs a Car instance with the specified details.
+     *
+     * @param buildYear the year the car was built
+     * @param maxSpeed the maximum speed of the car in km/h
+     * @param model the model name of the car
+     * @param color the color of the car
+     * @param isInsured true if the car is insured, false otherwise
+     * @param isDamaged true if the car is damaged, false otherwise
+     */
     public Car(int buildYear, int maxSpeed, String model, String color, boolean isInsured, boolean isDamaged) {
         this.buildYear = buildYear;
         this.maxSpeed = maxSpeed;
@@ -17,6 +32,14 @@ public class Car implements Comparable<Car> {
         this.isDamaged = isDamaged;
     }
 
+    /**
+     * Compares this car to another car by build year, and if equal, by maximum speed.
+     *
+     * @param other the other Car object to be compared
+     * @return a positive integer if this car is newer or has a lower max speed,
+     *         a negative integer if this car is older or has a higher max speed,
+     *         or zero if both build year and max speed are equal
+     */
     @Override
     public int compareTo(Car other) {
         if (this.buildYear > other.buildYear) { return 1; }
@@ -26,6 +49,13 @@ public class Car implements Comparable<Car> {
         return 0;
     }
 
+    /**
+     * Indicates whether some other object is "equal to" this one. Two cars are
+     * considered equal if all their fields have identical values.
+     *
+     * @param obj the reference object with which to compare
+     * @return true if this object is the same as the obj argument; false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -45,11 +75,24 @@ public class Car implements Comparable<Car> {
         return equality;
     }
 
+    /**
+     * Returns a hash code value for the car. This is computed based on
+     * the car's build year, max speed, model, color, insurance status,
+     * and damage status.
+     *
+     * @return a hash code value for this car
+     */
     @Override
     public int hashCode() {
         return Objects.hash(this.buildYear, this.maxSpeed, this.model, this.color, this.isInsured, this.isDamaged);
     }
 
+    /**
+     * Returns a string representation of the Car object, including build year,
+     * max speed, model, color, insurance, and damage status.
+     *
+     * @return a string representation of the car
+     */
     @Override
     public String toString() {
         return "Car {" +
